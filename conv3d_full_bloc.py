024d9077e2cffdf8564c06009ff616ad4c956a6a
Martinez Vincent <Vincent.MARTINEZ@tech-advantage.com>
2018-03-29 09:18:37 +0200

    Nouvelle methode via 3 convolution 3D

--- conv3d_full_bloc.py
+++ conv3d_full_bloc.py
@@ -39,32 +39,34 @@ def extract(img, imin, imax, jmin, jmax, kmin, kmax):
     subImage = array[imin:imax,jmin:jmax,kmin:kmax]
     return subImage
 
+ksize = 175//2
+isize = 152//2
+jsize = 152//2
+
 # on va travailler sur des morceaux de 22,19,19
-X = tf.placeholder(tf.float32, shape=[None, 175,152,152,1])
-Y = tf.placeholder(tf.float32, shape=[None, 175,152,152,1])
+X = tf.placeholder(tf.float32, shape=[None, ksize,jsize,isize,1])
+Y = tf.placeholder(tf.float32, shape=[None, ksize,jsize,isize,1])
 training = tf.placeholder(tf.bool)
 
 
 
 def cnn_model(x_train_data, keep_rate=0.7, seed=None):
     with tf.name_scope("layer_a"):
-        conv1 = tf.layers.conv3d(inputs=x_train_data, filters=24, kernel_size=[12, 12, 12], padding='same',
+        conv1 = tf.layers.conv3d(inputs=x_train_data, filters=32, kernel_size=[12, 12, 12], padding='same',
                                  activation=tf.nn.relu)
-        conv1_ = tf.layers.max_pooling3d(conv1, 4,1,padding='SAME')
-        conv2 = tf.layers.conv3d(inputs=conv1_,  filters=32, kernel_size=[7, 7, 7], padding='same', activation=tf.nn.relu)
-    with tf.device('/cpu:0'):
+        conv1_ = tf.layers.max_pooling3d(conv1, 2,1,padding='SAME')
+
+        conv2 = tf.layers.conv3d(inputs=conv1_,  filters=64, kernel_size=[7, 7, 7], padding='same', activation=tf.nn.relu)
         conv2_ = tf.layers.max_pooling3d(conv2, 2, 1, padding='SAME')
-        conv3 = tf.layers.conv3d(inputs=conv2_, filters=64, kernel_size=[5, 5, 5], padding='same', activation=tf.nn.relu)
-        conv3_ = tf.layers.max_pooling3d(conv3, 2, 1, padding='SAME')
-
-
-        with tf.name_scope("fully_con"):
-            dense = tf.layers.dense(inputs=conv3_, units=1024, activation=tf.nn.relu)
-         # (1-keep_rate) is the probability that the node will be kept
-            dropout = tf.layers.dropout(inputs=dense, rate=1-keep_rate, training=training)
-
-        with tf.name_scope("y_conv"):
-             y_conv = tf.layers.dense(dropout, 1, activation=None)
+        with tf.device('/cpu:0'):
+            conv3 = tf.layers.conv3d(inputs=conv2_, filters=128, kernel_size=[5, 5, 5], padding='same', activation=tf.nn.relu)
+            conv3_ = tf.layers.max_pooling3d(conv3, 2, 1, padding='SAME')
+            with tf.name_scope("fully_con"):
+                dense = tf.layers.dense(inputs=conv3_, units=1024, activation=tf.nn.relu)
+             # (1-keep_rate) is the probability that the node will be kept
+                dropout = tf.layers.dropout(inputs=dense, rate=1-keep_rate, training=training)
+            with tf.name_scope("y_conv"):
+                 y_conv = tf.layers.dense(dropout, 1, activation=None)
 
     return y_conv
 
@@ -79,6 +81,10 @@ saver = tf.train.Saver()
 
 load = 0
 
+
+
+
+
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
     if (load == 1):
@@ -88,29 +94,39 @@ with tf.Session() as session:
 
     for train in range(10000) :
 
-        for i in range(100):
+        for percent in range(100):
             rand = np.random.randint(320-batch_size)
             print("Random train = ", rand);
-            data = np.reshape(images[rand: rand +batch_size], [batch_size, 175, 152, 152, 1])
-            result = np.reshape(outputs[rand: rand+batch_size], [batch_size, 175, 152, 152, 1])/4
+            data = np.zeros((batch_size, ksize, isize, jsize, 1))
+            result = np.zeros((batch_size, ksize, isize, jsize, 1))
+
+            img = images[rand]
+            out = outputs[rand]
+            imin = np.random.randint(175 - ksize)
+            jmin = np.random.randint(152 - isize)
+            kmin = np.random.randint(152 - jsize)
+
+            for r in range(batch_size):
+                for i in range(ksize):
+                    for j in range(isize):
+                        for k in range(jsize):
+                            data[r, i, j, k, 0] = img[imin + i, jmin + j, k + kmin]
+                            value = out[imin + i, jmin + j, k + kmin]
+                            result[r, i, j, k, 0] = value / 4.
 
             loss, y = session.run([loss_fonction, yprev],
                                     feed_dict={X:data , Y: result, training: True})
-
-
-
-
             # Save the variables to disk.
-            save_path = saver.save(session, "model_full/conv_full.ckpt", global_step=train)
+            save_path = saver.save(session, "model_full/conv_full.ckpt", global_step=train*100+percent)
 
-            if i % 5 == 0:
+            if percent % 5 == 0:
                 file = open("compute.log", 'a')
-                log = "Train= " + str(train) + " Percent = " + str(i) + " Loss = " + str(
+                log = "Train= " + str(train) + " Percent = " + str(percent) + " Loss = " + str(
                     loss)+"\n"
 
                 file.writelines(log)
                 file.close()
-                print("Percent = ", i, " Loss = ", loss);
+                print("Percent = ", percent, " Loss = ", loss);
 
         # Testing
         rand = np.random.randint(80)
